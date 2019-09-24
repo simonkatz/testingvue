df7a54a0e29dc514cad7c3f84670ccab56e9c230
Simon Kateregga <spkateregga@gmail.com>
2019-09-24 18:47:29 -0500

    Change to vue template

---
+++ src/components/Counter.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <span class="count" v-text="count"></span>
+        
+        <button class="increment" @click="count++">Increment</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                count: 0
+            };
+        }
+    }
+</script>
